819d466c70d13255fb87e5c492ca967118a261db
phuockaito <huuphuocit1999@gmail.com>
2024-05-10 15:41:11 +0700

    fix: fuc

--- src/chain-id/index.ts
+++ src/chain-id/index.ts
@@ -1,13 +1,11 @@
-import { TOKEN } from "@/types";
-
 export * from "./5611";
 export * from "./97";
 
-export const getTokenChainId = async (chainId: number): Promise<Record<string, TOKEN>> => {
-    try {
-        const farms = (await import(`./${chainId}.ts`)).default;
-        return farms;
-    } catch (_) {
-        return {} as Record<string, TOKEN>;
-    }
-};
+// export const getTokenChainId = async (chainId: number): Promise<Record<string, TOKEN>> => {
+//     try {
+//         const farms = (await import(`./${chainId}.ts`)).default;
+//         return farms;
+//     } catch (_) {
+//         return {} as Record<string, TOKEN>;
+//     }
+// };
