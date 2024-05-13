b3da56d516a7e5a78c9c6353bf64e5030f7a5319
phuockaito <huuphuocit1999@gmail.com>
2024-05-13 10:44:34 +0700

    add: wallet

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,44 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { opBNBTestnet } from "viem/chains";
+import { createConfig, http } from '@wagmi/core'
+import { createClient } from "viem";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { coinbaseWallet, argentWallet, bifrostWallet, bitgetWallet, bitskiWallet, bitverseWallet, bloomWallet, coin98Wallet, injectedWallet, rainbowWallet, safeWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
 
-export const config = getDefaultConfig({
-    appName: "My RainbowKit App",
-    projectId: "YOUR_PROJECT_ID",
+const connectors = connectorsForWallets(
+    [
+        {
+            groupName: 'Recommended',
+            wallets: [
+                rainbowWallet,
+                walletConnectWallet,
+                injectedWallet,
+                safeWallet,
+                coinbaseWallet
+            ],
+        },
+        {
+            groupName: 'Others',
+            wallets: [
+                argentWallet,
+                bitgetWallet,
+                bifrostWallet,
+                bitskiWallet,
+                bitverseWallet,
+                bloomWallet,
+                coin98Wallet
+            ],
+        },
+    ],
+    {
+        appName: 'My RainbowKit App',
+        projectId: 'YOUR_PROJECT_ID',
+    }
+);
+
+export const config = createConfig({
     chains: [opBNBTestnet],
+    connectors,
+    client({ chain }) {
+        return createClient({ chain, transport: http() })
+    },
 });
