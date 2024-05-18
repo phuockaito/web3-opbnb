11c4f501ecd615b40bcbeb21a1095ab6e20e247f
phuockaito <huuphuocit1999@gmail.com>
2024-05-18 10:46:50 +0700

    clear code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,21 +3,6 @@ import { opBNBTestnet } from "viem/chains";
 import { bscTestnetToken, opBNBTestnetToken } from "@/chain-id";
 import { RenderTokenType } from "@/types";
 
-export const renderTokenUsdt = (chainId: number) => {
-    if (chainId === opBNBTestnet.id) return opBNBTestnetToken.USDT;
-    return bscTestnetToken.USDT;
-};
-
-export const renderTokenUsdb = (chainId: number) => {
-    if (chainId === opBNBTestnet.id) return opBNBTestnetToken.USDB;
-    return bscTestnetToken.USDB;
-};
-
-export const renderTokenSusdb = (chainId: number) => {
-    if (chainId === opBNBTestnet.id) return opBNBTestnetToken.SUSDB;
-    return bscTestnetToken.SUSDB;
-};
-
 export const RENDER_TOKEN = (chainId: number): RenderTokenType => {
     switch (chainId) {
         case opBNBTestnet.id:
