7cb2f1f33372ba8d33b7914e14467121844e6b6d
phuockaito <huuphuocit1999@gmail.com>
2024-05-18 11:07:48 +0700

    add: fuc get token

--- src/chain-id/index.ts
+++ src/chain-id/index.ts
@@ -1,11 +1,13 @@
+import type { RenderTokenType } from "@/types";
+
 export * from "./5611";
 export * from "./97";
 
-// export const getTokenChainId = async (chainId: number): Promise<Record<string, TOKEN>> => {
-//     try {
-//         const farms = (await import(`./${chainId}.ts`)).default;
-//         return farms;
-//     } catch (_) {
-//         return {} as Record<string, TOKEN>;
-//     }
-// };
+export const getTokenChainId = async (chainId: number): Promise<RenderTokenType> => {
+    try {
+        const farms = (await import(`./${chainId}.ts`)).default;
+        return farms;
+    } catch (_) {
+        return {} as RenderTokenType;
+    }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dynamicImport from "vite-plugin-dynamic-import";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dynamicImport({
+            filter(id) {
+                if (id.includes(`./${id}.ts`)) {
+                    return true;
+                }
+            },
+        }),
+    ],
     resolve: {
         alias: [{ find: "@", replacement: "/src" }],
     },
