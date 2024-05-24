c3d0ef083ac42f4833306360b8dac564660a4bad
phuockaito <huuphuocit1999@gmail.com>
2024-05-24 14:58:09 +0700

    add: Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 import { opBNBTestnet } from "viem/chains";
 import { WagmiProvider } from "wagmi";
@@ -36,6 +37,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                         })}
                     >
                         <Pages />
+                        <Analytics />
                     </RainbowKitProvider>
                 </QueryClientProvider>
             </WagmiProvider>
