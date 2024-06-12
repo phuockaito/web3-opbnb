aae91467df6a7818bc42a91b3c443704fa53c21b
phuockaito <huuphuocit1999@gmail.com>
2024-06-12 10:53:10 +0700

    save

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,12 +2,12 @@ import { opBNBTestnet } from "viem/chains";
 
 export type NAME_TOKEN_TYPE = "SUSDB" | "USDB" | "USDT";
 
+export const INITIAL_CHAIN = opBNBTestnet;
 export const NAME_METHOD_STAKE = "Stake";
 export const NAME_METHOD_UN_STAKE = "Unstake";
 export const NAME_METHOD_BUY = "Buy";
 export const NAME_METHOD_SELL = "Sell";
 
-export const INITIAL_CHAIN = opBNBTestnet;
 export const PROJECT_ID = "82b3a1a0-ecb9-47af-a03d-1bfd44694b94";
 export const CLIENT_KEY = "cdJPVDSGlMgBZgWeUnuYKC3JaQIWAIjn3Cjs6pmh";
 export const APP_ID = "b7207178-6693-45f6-97eb-8384b9fd51f7";
