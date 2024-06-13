8c558856e01643b1f7aa77ba0fc9812c03ae1698
phuockaito <huuphuocit1999@gmail.com>
2024-06-13 09:37:45 +0700

    add: type

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
-import { opBNBTestnet } from "viem/chains";
+import { Chain, opBNBTestnet } from "viem/chains";
 
 export type NAME_TOKEN_TYPE = "SUSDB" | "USDB" | "USDT";
 
-export const INITIAL_CHAIN = opBNBTestnet;
-export const NAME_METHOD_STAKE = "Stake";
-export const NAME_METHOD_UN_STAKE = "Unstake";
-export const NAME_METHOD_BUY = "Buy";
-export const NAME_METHOD_SELL = "Sell";
+export const INITIAL_CHAIN: Chain = opBNBTestnet;
+export const NAME_METHOD_STAKE: string = "Stake";
+export const NAME_METHOD_UN_STAKE: string = "Unstake";
+export const NAME_METHOD_BUY: string = "Buy";
+export const NAME_METHOD_SELL: string = "Sell";
 
-export const PROJECT_ID = "82b3a1a0-ecb9-47af-a03d-1bfd44694b94";
-export const CLIENT_KEY = "cdJPVDSGlMgBZgWeUnuYKC3JaQIWAIjn3Cjs6pmh";
-export const APP_ID = "b7207178-6693-45f6-97eb-8384b9fd51f7";
+export const PROJECT_ID: string = "82b3a1a0-ecb9-47af-a03d-1bfd44694b94";
+export const CLIENT_KEY: string = "cdJPVDSGlMgBZgWeUnuYKC3JaQIWAIjn3Cjs6pmh";
+export const APP_ID: string = "b7207178-6693-45f6-97eb-8384b9fd51f7";
